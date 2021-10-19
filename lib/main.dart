70eef4d0fac512a4888898390fb257b5e4076dd0
reydaniels39 <reydaniels@outlook.com>
2021-10-18 22:02:05 -0500

    Se agregó la carpeta assets, se modifico el archivo pubspec para reconocerla y se empezó el código de algunas páginas, rutas y widgets

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_eats/src/pages/login_page.dart';
+import 'package:flutter_eats/src/routes/routes.dart';
+
+void main() => runApp(FlutterEats());
+
+class FlutterEats extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,                      //DebugBanner
+      title: 'Flutter Eats',                                  //Título de la app
+      theme: ThemeData(                                       //Temas para la app
+        primaryColor: Color.fromRGBO(0, 255, 255, 1),         //Color Primario para el Tema
+        colorScheme: ThemeData().colorScheme.copyWith(
+          primary: Color.fromRGBO(0, 255, 255, 1),            //Color Primario para el Tema
+        ),
+      ),
+      initialRoute: '/',                                      //Ruta Inicial
+      routes: getRoutes(),                                    //Obtener Rutas para navegación de la app
+      onGenerateRoute: (settings){                            //En caso de ruta no encontrada...
+        return MaterialPageRoute(
+          builder: (BuildContext context) => LoginPage(),     //...Cargar Login Page
+        );
+      },
+    );
+  }
+}
---
+++ lib/src/widgets/input_nombre.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class InputNombre extends StatefulWidget {
+  const InputNombre({Key? key}) : super(key: key);
+
+  @override
+  _InputNombreState createState() => _InputNombreState();
+
+}
+
+class _InputNombreState extends State<InputNombre> {
+  String nombre = '';                                                   //Variable que guarda el nombre del Input
+
+  @override
+  Widget build(BuildContext context) {
+    var _textStyle = TextStyle(color: Colors.white, fontSize: 20);      //Variable de Estilo de Texto
+
+    return TextField(                                                   //Input
+      style: _textStyle,                                                //Estilo del Texto que se escribe en el input
+      textCapitalization: TextCapitalization.words,                     //Mayúscula la inicial de cada palabra
+      decoration: InputDecoration(
+        hintText: 'Nombre o Usuario',                                   //Placeholder del input
+        hintStyle: _textStyle,                                          //Estilo del texto del Placeholder
+        icon: Icon(Icons.account_circle_rounded),                       //Icono del input
+      ),
+      onChanged: (valor){                                               //Actualizar la variable con cada Cambio de valor
+        setState(() {
+          nombre = valor;
+        });    
+      },
+      onSubmitted: (valor){                                             //Actualizar la variable al pulsar la palomita del teclado del input
+        nombre = valor;
+      },
+    );
+  }
+}
