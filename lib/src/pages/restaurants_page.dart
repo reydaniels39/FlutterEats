70c5a69bc48b8bf0fb294402066abe730dc204cc
Ernestogtz99 <82416775+Ernestogtz99@users.noreply.github.com>
2021-10-21 15:43:25 -0500

    Add files via upload

--- lib/src/pages/restaurants_page.dart
+++ lib/src/pages/restaurants_page.dart
@@ -5,7 +5,9 @@ import 'package:flutter_eats/src/providers/inputs_login_provider.dart';
 class RestaurantsPage extends StatelessWidget {
   const RestaurantsPage({Key? key}) : super(key: key);
 
-  static final restaurantes = ["Italiannis", "Burguer King", "Pizza Hut", "Sushi Roll"];        //Prueba
+  //Prueba
+  static final restaurantes = ["Italiannis", "Burguer King", "Pizza Hut", "Sushi Roll", "Restaurante1", "Restaurante2", "Restaurante3", "Restaurante 4", "Restaurante5"];        
+  static final categorias = ["Pizza, Pastas", "Hamburguesas, Comida rápida", "Pizza", "Sushi, Oriental", "De todo", "De todo", "De todo", "De todo", "De todo"];        
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +17,9 @@ class RestaurantsPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),   //Margen para nuestra columna
+          margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),     //Margen para nuestra columna 
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text('Bienvenido ${inputsLoginProvider.nombre}',                //Mensaje de bienvenida al usuario
                 style: TextStyle( 
@@ -24,14 +27,14 @@ class RestaurantsPage extends StatelessWidget {
                   fontSize: 25,
                   fontWeight: FontWeight.w500,
                 ),
-              ),
-              ListView(                                                       //Lista de restaurantes
-                //children: listaRestaurantes()                                //Método que obtiene una lista con los widgets
-                children: <Widget>[                                            //No funciona :s
-                  Text('Holaaa', style: TextStyle(color: Colors.black)),
-                  Text('Como estas'),
-                  Text('yo bien y tu'),
-                ],
+              ), 
+              SizedBox(height: 40),
+              Text('Restaurantes cerca de ti'),
+              Divider(color: Colors.black),
+              Expanded(                                                           //Para que nuestro ListView abarque todo el espacio posible
+                child: ListView(                                                  //Lista de restaurantes
+                  children: listaRestaurantes()                                   //Método que obtiene una lista con los widgets
+                ),
               ),
             ],
           ),
@@ -40,12 +43,22 @@ class RestaurantsPage extends StatelessWidget {
     );
   }
 
-  List <Widget> listaRestaurantes(){            //No funciona :s
-    var widgets = restaurantes.map((index){
+  List <Widget> listaRestaurantes(){            
+    var widgets = restaurantes.map((index){                                           //Por cada elemento que tenemos en la lista 'restaurantes'
       return Column(
         children: <Widget>[
-          ListTile(
-            title: Text('$index'),
+          Card(                                                                       //Creamos una carta por restaurante
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+            
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(index),
+                  subtitle: Text('Categoria: Pizzas, Pasta'),
+                )
+              ],
+            ),
           ),
         ],
       );
